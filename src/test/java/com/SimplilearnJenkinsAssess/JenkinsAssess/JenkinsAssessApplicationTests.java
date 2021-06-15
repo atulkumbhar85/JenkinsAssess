9a0a67211ae9f92a290877b2126d4722befcfffb
student <atul.kumbhar1985@gmail.com>
2021-06-15 14:36:56 +0000

    First file added

---
+++ src/test/java/com/SimplilearnJenkinsAssess/JenkinsAssess/JenkinsAssessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SimplilearnJenkinsAssess.JenkinsAssess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsAssessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
