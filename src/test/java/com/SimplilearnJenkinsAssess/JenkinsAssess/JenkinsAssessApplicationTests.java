c9014f93d65cad4ba0c0b412f8646b4d6934b03b
atulkumbhar85 <67327357+atulkumbhar85@users.noreply.github.com>
2021-10-19 13:18:18 +0530

    Update JenkinsAssessApplicationTests.java

--- src/test/java/com/SimplilearnJenkinsAssess/JenkinsAssess/JenkinsAssessApplicationTests.java
+++ src/test/java/com/SimplilearnJenkinsAssess/JenkinsAssess/JenkinsAssessApplicationTests.java
@@ -1,7 +1,18 @@
 package com.SimplilearnJenkinsAssess.JenkinsAssess;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
 class JenkinsAssessApplicationTests {
