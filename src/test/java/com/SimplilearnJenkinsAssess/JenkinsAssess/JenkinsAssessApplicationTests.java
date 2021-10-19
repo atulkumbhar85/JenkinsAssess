4ea29f054bb8186dcc8a1dc18652bdb03db6a557
atulkumbhar85 <67327357+atulkumbhar85@users.noreply.github.com>
2021-10-19 13:14:14 +0530

    Update JenkinsAssessApplicationTests.java

--- src/test/java/com/SimplilearnJenkinsAssess/JenkinsAssess/JenkinsAssessApplicationTests.java
+++ src/test/java/com/SimplilearnJenkinsAssess/JenkinsAssess/JenkinsAssessApplicationTests.java
@@ -5,8 +5,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinsAssessApplicationTests {
+	
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
+	public void testGreeting() throws Exception {
+		ResponseEntity<String> entity = restTemplate
+				.getForEntity("http://localhost:" + this.port + "/", String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+	}
 	void contextLoads() {
 	}
 
